42a7d724c1c45f2a0ab261cda93bedcb02c8b675
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-05 16:51:38 -0600

    Add Package Express

---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        Console.WriteLine("\nPlease enter the package weight:");
+        int weight = Convert.ToInt32(Console.ReadLine());
+
+        if (weight > 50)
+        {
+            Console.WriteLine("Error: Package is too heavy to be shipped via Package Express.");
+        }
+        else
+        {
+            Console.WriteLine("\nPlease enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            if ((width + height + length) > 50)
+            {
+                Console.WriteLine("Error: Package is too big to be shipped via Package Express");
+            }
+            else
+            {
+                float cost = (float)((height * width * length) * weight) / 100;
+                Console.WriteLine("\nYour estimated total for shipping this package is: $" + cost);
+                Console.WriteLine("Thank you!");
+            }
+        }
+        Console.Read();
+    }
+}
