3175fbf28d613fc4874878fd87f3804d6547e23a
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-05 14:29:38 -0600

    Add Insurance Approval

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(true && false);
+        Console.WriteLine(true && true);
+        Console.WriteLine(false && false);
+
+        Console.WriteLine("\n");
+        Console.WriteLine(true || false);
+        Console.WriteLine(true || true);
+        Console.WriteLine(false || false);
+
+        Console.WriteLine("\n");
+        Console.WriteLine(true == false);
+        Console.WriteLine(true == true);
+        Console.WriteLine(false == false);
+
+        Console.WriteLine("\n");
+        Console.WriteLine(true != false);
+        Console.WriteLine(true != true);
+        Console.WriteLine(false != false);
+
+        Console.WriteLine("\n");
+        Console.WriteLine(true ^ false);
+        Console.WriteLine(true ^ true);
+        Console.WriteLine(false ^ false);
+
+        Console.Read();
+    }
+}
+
---
+++ CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Car Insurance Approval Program");
+
+        Console.WriteLine("\nHow old are you?");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nHave you ever had a DUI? (Type \"True\" or \"False\")");
+        bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine("\nHow many speeding tickets do you have?");
+        int tickets = Convert.ToInt32(Console.ReadLine());
+
+        bool approved = age > 15 && DUI == false && tickets <= 3;
+        Console.WriteLine("\nQualified?");
+        Console.WriteLine(approved);
+        Console.Read();
+    }
+}
