d3511b5f8803541e542fe3f50dad0792604c7884
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-11 11:14:59 -0600

    Add Methods

---
+++ Methods/Methods/Math.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Math
+    {
+        public static string Multiply(int input)
+        {
+            int result = input * 8;
+            string output = input + " times 8 is " + result;
+            return output;
+        }
+
+        public static string Addition(int input)
+        {
+            int result = input + 12;
+            string output = input + " plus 12 is " + result;
+            return output;
+        }
+
+        public static string Subtraction(int input)
+        {
+            int result = input - 5;
+            string output = input + " minus 5 is " + result;
+            return output;
+        }
+
+        //public static int Multiply(int input)
+        //{
+        //    return input * 8;
+        //}
+
+        //public static int Addition(int input)
+        //{
+        //    return input + 12;
+        //}
+
+        //public static int Subtraction(int input)
+        //{
+        //    return input - 5;
+        //}
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a whole number and we'll do some math on it!:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Multiply(input));
+            Console.WriteLine(Math.Addition(input));
+            Console.WriteLine(Math.Subtraction(input));
+
+            Console.Read();
+        }
+    }
+}
