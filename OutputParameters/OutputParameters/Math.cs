d6775833872ce1c7e76bfd7fcc74b9d1f0bb08e8
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-12 16:14:08 -0600

    Add OutputParameters

---
+++ OutputParameters/OutputParameters/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    class Math
+    {
+        public static void DoMath(int x, out int thrownVar)
+        {
+            thrownVar = x / 2;
+        }
+
+        public static void DoMath(decimal y, out decimal thrownVar2)
+        {
+            thrownVar2 = y * 4.12m;
+        }
+    }
+}
---
+++ OutputParameters/OutputParameters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a whole number and we'll divide it by 2:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Math.DoMath(x, out int thrownVar);
+            Console.WriteLine(thrownVar);
+
+
+            Console.WriteLine("\nGive me a decimal value and we'll multiply it by 4.12:");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+
+            Math.DoMath(y, out decimal thrownVar2);
+            Console.WriteLine(thrownVar2);
+
+            Console.Read();
+        }
+    }
+}
