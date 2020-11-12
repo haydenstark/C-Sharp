9ddaa512dc66320efc4398670e7c51352861fa6d
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-11 23:35:56 -0600

    Edit Methods

--- Methods/Methods/Math.cs
+++ Methods/Methods/Math.cs
@@ -8,40 +8,40 @@ namespace Methods
 {
     public class Math
     {
-        public static string Multiply(int input)
+        public static int Multiply(int input)
         {
-            int result = input * 8;
-            string output = input + " times 8 is " + result;
-            return output;
+            return input * 8;
         }
 
-        public static string Addition(int input)
+        public static int Addition(int input)
         {
-            int result = input + 12;
-            string output = input + " plus 12 is " + result;
-            return output;
+            return input + 12;
         }
 
-        public static string Subtraction(int input)
+        public static int Subtraction(int input)
         {
-            int result = input - 5;
-            string output = input + " minus 5 is " + result;
-            return output;
+            return input - 5;
         }
 
-        //public static int Multiply(int input)
+        //public static string Multiply(int input)
         //{
-        //    return input * 8;
+        //    int result = input * 8;
+        //    string output = input + " times 8 is " + result;
+        //    return output;
         //}
 
-        //public static int Addition(int input)
+        //public static string Addition(int input)
         //{
-        //    return input + 12;
+        //    int result = input + 12;
+        //    string output = input + " plus 12 is " + result;
+        //    return output;
         //}
 
-        //public static int Subtraction(int input)
+        //public static string Subtraction(int input)
         //{
-        //    return input - 5;
+        //    int result = input - 5;
+        //    string output = input + " minus 5 is " + result;
+        //    return output;
         //}
     }
 }
