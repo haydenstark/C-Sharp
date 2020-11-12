2a17583c144668c5bf82808bc6a9e57b748788a0
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-12 11:48:24 -0600

    Add MethodOverloading

---
+++ MethodOverloading/MethodOverloading/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Math
+    {
+        public static int DoMath(int input)
+        {
+            return input * 4;
+        }
+        public static int DoMath(decimal input)
+        {
+            return Convert.ToInt32( input / 2.5m );
+        }
+        public static int DoMath(string input)
+        {
+            int intInput = Convert.ToInt32(input);
+            return intInput - 12;
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a whole number and we'll multiply it by 4:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.DoMath(userInput));
+
+            Console.WriteLine("\nGive me a decimal value and we'll divide it by 2.5 (and round it to a whole value):");
+            decimal userInput2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(Math.DoMath(userInput2));
+
+            Console.WriteLine("\nGive me a whole number and we'll subtract 12 from it:");
+            string userInput3 = Console.ReadLine();
+            Console.WriteLine(Math.DoMath(userInput3));
+
+            Console.WriteLine("\nOne last time - give me a number (preferably a whole number) and we'll repeat all three operations:");
+            string asString = Console.ReadLine();
+            int asInt = Convert.ToInt32(asString);
+            decimal asDec = Convert.ToDecimal(asString);
+            Console.WriteLine(Math.DoMath(asInt));
+            Console.WriteLine(Math.DoMath(asDec));
+            Console.WriteLine(Math.DoMath(asString));
+
+            Console.Read();
+        }
+    }
+}
