efe043d99d96d5d865d17f2e87bc83ad6f22e266
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-12 13:24:21 -0600

    Add void

---
+++ Void/Void/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void
+{
+    public class Math
+    {
+        public static void DoMath(int a, int b)
+        {
+            int product = a * 4;
+            //  Wasn't sure if we're meant to display results, but if needed:
+            // Console.WriteLine(product);
+
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Void/Void/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Math variableMath = new Math();      - instantiating, but commenting it out considering the class only has a method and isn't being populated with anything
+
+            Math.DoMath(6, 7);
+            
+            Math.DoMath(a: 5, b:8);             // named parameters
+
+            Console.Read();
+        }
+    }
+}
