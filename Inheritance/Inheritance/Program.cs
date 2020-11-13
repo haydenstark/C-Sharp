640b5c5948ae45cb76e7c70d0d36a54a3bffb55a
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-13 14:02:03 -0600

    Add Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+
+            //Employee joe = new Employee() { FirstName = "Joe", LastName = "Johnson", ID = 72 };
+            //joe.SayName();
+            //Console.WriteLine(joe.ID);
+
+            Console.Read();
+        }
+    }
+}
