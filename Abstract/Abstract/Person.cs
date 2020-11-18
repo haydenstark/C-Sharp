88852e8c7eb38327dee66c138767c71541c5796e
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-18 09:27:27 -0600

    Add Abstract

---
+++ Abstract/Abstract/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+
+
+
+        //public virtual void SayName()
+        //{
+        //    Console.WriteLine(firstName + " " + lastName);
+        //}
+    }
+}
