6b993ee90bf6629378f4a2ad6e66c6eeade361cc
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-18 11:07:23 -0600

    Add OverloadingOperators

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public static bool operator== (Employee a, Employee b)
+        {
+            return a.ID == b.ID;
+        }
+        public static bool operator!= (Employee a, Employee b)
+        {
+            return a.ID != b.ID;
+        }
+    }
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Barb = new Employee() { Name = "Barb", ID = 101 };
+            Employee Nick = new Employee() { Name = "Nick", ID = 102 };
+            Employee Leroy = new Employee() { Name = "Leroy Jenkins", ID = 101 };
+
+            Console.WriteLine("Is equal:");
+            Console.WriteLine(Barb == Leroy);
+            Console.WriteLine(Nick == Barb);
+
+            Console.WriteLine("\nIs NOT equal:");
+            Console.WriteLine(Barb != Leroy);
+            Console.WriteLine(Nick != Barb);
+            Console.Read();
+        }
+    }
+}
