3a85384ebedd4953b20071b07fd155fd4579aceb
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-20 14:38:56 -0600

    Add Structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number pi = new Number() { Amount = 3.14159m };
+
+            Console.WriteLine(pi.Amount);
+            Console.Read();
+        }
+    }
+}
