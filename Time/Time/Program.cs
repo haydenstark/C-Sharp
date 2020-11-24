3f1d5bf417debdb7238715f16b01c34325d46ca0
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-24 11:45:23 -0600

    Add Time

---
+++ Time/Time/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            Console.WriteLine("Give me a number:");
+            double input = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nIn {0} hours, it will be:", input);
+            Console.WriteLine(now.AddHours(input));
+
+            Console.Read();
+        }
+    }
+}
