f69e9b73bd180808322894bf4e85e439d3d8accb
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-24 11:14:09 -0600

    Add Logs

---
+++ Logs/Logs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number and we'll log it:");
+            string input = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\james\Documents\Logs\log.txt", input);
+
+            string txtfile = File.ReadAllText(@"C:\Users\james\Documents\Logs\log.txt");
+            Console.WriteLine(txtfile);
+
+            Console.Read();
+        }
+    }
+}
