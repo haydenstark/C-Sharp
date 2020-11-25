04b73d485fd27e52a3aa8b1c9a883d241e72bb46
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-25 12:02:54 -0600

    Add Exceptions

---
+++ Exceptions/Exceptions/Age_DNE_Exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class Age_DNE_Exception : Exception
+    {
+        public Age_DNE_Exception()
+            : base() { }
+        public Age_DNE_Exception(string message)
+            : base(message) { }
+    }
+}
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Please enter your age (in years):");
+
+                bool validAnswer = false;
+                int age = 0;
+                while (!validAnswer)
+                {
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter a whole number");
+                }
+
+                if (age < 0) throw new NegativeAgeException();
+                if (age == 0) throw new Age_DNE_Exception();
+
+                DateTime now = DateTime.Now;
+                age *= -1;          // flipping to a negative number so when AddYears method is used, the years are instead subtracted
+                Console.WriteLine("You were most likely born in: " + now.AddYears(age).Year);
+            }
+            catch (Age_DNE_Exception)
+            {
+                Console.WriteLine("Age can not be zero. Please restart application.");
+            }
+            catch (NegativeAgeException)
+            {
+                Console.WriteLine("Age can not be negative. Please restart application.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong, please restart application.");
+            }
+
+
+            Console.Read();
+        }
+    }
+}
