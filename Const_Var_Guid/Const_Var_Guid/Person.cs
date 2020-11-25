31105bfed89277a26c5062a3a47b9b621d82e9ce
Hayden Stark <68758748+haydenstark@users.noreply.github.com>
2020-11-25 09:53:39 -0600

    Add Const_Var_Guid

---
+++ Const_Var_Guid/Const_Var_Guid/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var_Guid
+{
+    public class Person
+    {
+        public Person(string name, string year)
+        {
+            Name = name;
+            Year = year;
+        }
+        public Person(string name) : this(name, "is a first-year student")
+        {
+        }
+
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public Guid ID { get; set; }
+    }
+}
---
+++ Const_Var_Guid/Const_Var_Guid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var_Guid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string hogwarts = "Hogwarts School of Witchcraft and Wizardry";
+            var harry = new Person("Harry Potter");
+
+            Console.WriteLine(harry.Name);
+            Console.WriteLine(harry.Year);
+            Console.WriteLine(hogwarts);
+
+            harry.ID = Guid.NewGuid();
+            Console.WriteLine(harry.ID);
+
+            Console.Read();
+        }
+    }
+}
